31930e42aa163d0f3d4e0be8ae995c13c0aaf01e
theta <theta@gmail.com>
2019-09-17 11:04:24 +0900

    1

---
+++ day2.js
@@ -0,0 +1,12 @@
+var length=16;
+var lastName="Johnson";
+
+var x={firstName:"John",lastName: "Doe"};
+
+console.log(x);
+
+var x=16+"Volvo";
+console.log(x);
+
+var b=16+4+"Volvo";
+console.log(b);
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("message");
+console.log("message 2");
+
+array.forEach(element => {
+    
+});
